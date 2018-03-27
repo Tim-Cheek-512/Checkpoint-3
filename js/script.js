e94d7dd678fbb3137198886a5064e46b22f596fa
Tim <timcheek99@gmail.com>
2018-03-27 18:59:12 -0500

    starting checkpoint 3

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use Strict"
+$(document).ready(function(){
+  // Start of making each button clickable.....
+  // Button 1 function start...
+    var button1Function = $('#but1').on('click', function(){
+        alert('Button 1 clicked');
+    });
+
+
+  // ......button 1 end
+
+  // Button 2 function start...
+
+
+
+  // ......button 2 end
+
+  // Button 3 function start...
+
+
+
+  // ......button 3 end
+  // Button 4 function start...
+
+
+
+  // ......button 4 end
+
+
+
+})
